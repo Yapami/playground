9fc5a1bc09213e691d3ad4fe064f1ae0b60764d2
Volodymyr Novikov <vlrnov@gmail.com>
2021-06-27 12:40:49 +0300

    Replaced test expectations for float comparison

--- phone-number/TestPhoneNumber.cpp
+++ phone-number/TestPhoneNumber.cpp
@@ -6,17 +6,17 @@
 TEST(PhoneNumber, distance_generation)
 {
     auto d = distances();
-    EXPECT_NEAR(d[0][8], 1.0f, 0.0001);
+    EXPECT_FLOAT_EQ(d[0][8], 1.0f);
     const float one_to_zero = std::pow(10, 0.5f);
-    EXPECT_NEAR(d[1][0], one_to_zero, 0.0001);
+    EXPECT_FLOAT_EQ(d[1][0], one_to_zero);
 }
 
 TEST(PhoneNumber, simple_case)
 {
     float r = fastest_input({1, 2});
-    EXPECT_NEAR(r, 4.0, 0.001);
+    EXPECT_FLOAT_EQ(r, 4.0);
     r = fastest_input({1, 2, 9});
-    EXPECT_NEAR(r, 5.0, 0.001);
+    EXPECT_FLOAT_EQ(r, 5.0);
     r = fastest_input({1, 2, 9, 4});
-    EXPECT_NEAR(r, 5.0 + std::pow(2.0, 0.5), 0.001);
+    EXPECT_FLOAT_EQ(r, 5.0 + std::pow(2.0, 0.5));
 }
