fe1eac89e8e2beb3e9220e561d2f749c03cb5afd
Volodymyr Novikov <vlrnov@gmail.com>
2021-10-11 06:16:25 +0300

    Extended jumping on the clouds solution

--- hackerrank/jumping-on-the-clouds.cpp
+++ hackerrank/jumping-on-the-clouds.cpp
@@ -15,8 +15,34 @@ int jumpingOnClouds(std::vector<int> c, int k)
     return e;
 }
 
+int jumpingOnClouds(const std::vector<int> &c)
+{
+    int j = 0;
+    auto s = c.size() - 2;
+    size_t i;
+    for (i = 0; i < s; ++j)
+    {
+        if (c[i + 2] == 0)
+        {
+            i += 2;
+        }
+        else
+        {
+            ++i;
+        }
+    }
+    if (i < c.size() - 1)
+    {
+        ++j;
+    }
+    return j;
+}
+
 TEST(JumpingOnTheClouds, sample)
 {
     EXPECT_EQ(jumpingOnClouds({0, 0, 1, 0}, 2), 96);
     EXPECT_EQ(jumpingOnClouds({0, 0, 1, 0, 0, 1, 1, 0}, 2), 92);
+    EXPECT_EQ(jumpingOnClouds({0, 1, 0, 0, 0, 1, 0}), 3);
+    EXPECT_EQ(jumpingOnClouds({0, 0, 0, 0, 1, 0}), 3);
+    EXPECT_EQ(jumpingOnClouds({0, 0, 0, 1, 0, 0}), 3);
 }
